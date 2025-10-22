f04eb9e58af4579fea74aceb6c02309dd29155ba
Leonhelm <qwiray@gmail.com>
2025-10-22 10:14:17 +0300

    wip

--- cooking-day-bot.js
+++ cooking-day-bot.js
@@ -1,7 +1,26 @@
 const TG_BOT_API_TOKEN = process.env.TG_BOT_API_TOKEN;
+const API = `https://api.telegram.org/bot${TG_BOT_API_TOKEN}`;
+
+let offset = 0;
+
+const getUpdates = async () => {
+  const res = await fetch(`${API}/getUpdates?timeout=30&offset=${offset}`);
+  const data = await res.json();
+  return data.result;
+};
+
+const sendMessage = async (chat_id, text) => {
+  await fetch(`${API}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ chat_id, text }),
+  });
+};
 
 const run = async () => {
-  console.log('I BEELIVE!', Boolean(TG_BOT_API_TOKEN));
+  const updates = await getUpdates();
+
+  console.log("UPDATES:", JSON.stringify(updates, null, 2));
 };
 
 await run();
