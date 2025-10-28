4fde39a135d87034a81b0c65a6067095d568cdea
Leonhelm <qwiray@gmail.com>
2025-10-28 14:50:10 +0300

    wip

--- cooking-day-bot.js
+++ cooking-day-bot.js
@@ -50,17 +50,39 @@ const getUpdates = async () => {
   return data.result;
 };
 
-const sendMessage = async (chat_id, text) => {
+const sendMessage = async (chatId, text) => {
   await fetch(`${API}/sendMessage`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ chat_id, text }),
+    body: JSON.stringify({ chat_id: chatId, text }),
   });
 };
 
+const deleteMessage = async (chatId, messageId) => {
+  const res = await fetch(`${API}/deleteMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ chat_id: chatId, message_id: messageId }),
+  });
+  return res.json();
+};
+
 const run = async () => {
-  // const updates = await getUpdates();
-  // console.log("UPDATES:", JSON.stringify(updates, null, 2));
+  const updates = await getUpdates();
+
+  for (const update of updates) {
+    const { update_id: updateId, message } = update;
+    const { message_id: messageId, from, chat, date, text } = message;
+    const { id: userId } = from;
+    const {
+      id: chatId,
+      first_name: firstName,
+      last_name: lastName,
+      username,
+    } = chat;
+
+    await deleteMessage(chatId, messageId);
+  }
 };
 
 await run();
